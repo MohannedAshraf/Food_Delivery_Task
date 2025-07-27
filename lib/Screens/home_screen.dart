4f0284fe8723c2ae6f8d0da146bff2062afb0b10
mohanned <MohannedAshraf GitHub>
2025-07-27 03:28:47 +0300

    task exept products

---
+++ lib/Screens/home_screen.dart
@@ -0,0 +1,137 @@
+// ignore_for_file: deprecated_member_use
+
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
+import 'package:food_delivery_app/core/utils/asset_image.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          "Food Delivery",
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.shopping_cart_outlined),
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              height: 40,
+
+              decoration: BoxDecoration(
+                color: Colors.red.shade50,
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: TextField(
+                decoration: InputDecoration(
+                  contentPadding: EdgeInsets.zero,
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                    borderSide: BorderSide(
+                      color: Colors.grey.withOpacity(0.1), // رصاصي خفيف جدًا
+                      width: 1,
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                    borderSide: BorderSide(
+                      color: Colors.grey.withOpacity(
+                        0.1,
+                      ), // نفس اللون عند التركيز
+                      width: 1,
+                    ),
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                    borderSide: BorderSide(
+                      color: Colors.grey.withOpacity(0.1),
+                      width: 1,
+                    ),
+                  ),
+
+                  prefixIcon: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: Row(
+                      children: [
+                        Icon(Icons.search, color: Colors.red.shade300),
+                        SizedBox(width: 5),
+                        Text(
+                          "Search for foods",
+                          style: TextStyle(
+                            color: Colors.red.shade300,
+                            fontSize: 15,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 15),
+            CarouselSlider(
+              options: CarouselOptions(
+                autoPlay: false,
+                height: 250,
+                viewportFraction: 1,
+              ),
+              items:
+                  items.map((item) {
+                    return Builder(
+                      builder: (BuildContext context) {
+                        return Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Container(
+                              width: MediaQuery.of(context).size.width,
+                              height: 150,
+                              margin: EdgeInsets.symmetric(horizontal: 5.0),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                image: DecorationImage(
+                                  image: AssetImage(item['image']!),
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 10),
+
+                            Text(
+                              item['text']!,
+                              style: TextStyle(fontSize: 20.0),
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                  }).toList(),
+            ),
+            Text(
+              "Featured Items",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+final List<Map<String, String>> items = [
+  {'image': Myimages.burger, 'text': "burger"},
+  {'image': Myimages.nescalop, 'text': "nescalop"},
+  {'image': Myimages.sandwitch, 'text': "sandwitch"},
+];
